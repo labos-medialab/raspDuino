491c8a9cdc2ecbe0768f22027faf2c10e3b331fe
Labos Medialab/Hackerspace projects <v.alentturkovic@gmail.com>
2014-10-08 11:01:04 +0200

    Update test.ino
    pozdrav s gita

--- src/test.ino
+++ src/test.ino
@@ -1,8 +1,17 @@
 void setup()
 {
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  delay(20);
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
 
